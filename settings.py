54c7d8b96aae9305df084f59d7a1647e4167c305
Sébastien Tosi <sebastien.tosi@gmail.com>
2024-10-24 17:31:00 +0200

    Delete settings.py

--- settings.py
+++
@@ -1,5 +0,0 @@
-filename = 'D:/Projects/UPF/Widefield_TIRF/_TestImages/Test_5x5_pre_C1.tif'
-filename2 = 'D:/Projects/UPF/Widefield_TIRF/_TestImages/Test_5x5_pre_C2.tif'
-start_frame, end_frame = 25, 500
-color_codes = ['green', 'yellow', 'red']
-loadchan2 = True
