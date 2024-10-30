4be22d761ff42ef262eb403cf2ac26e07d36d484
SebastienTs <sebastien.tosi@gmail.com>
2024-10-30 12:34:40 +0100

    Removed Windows specific files and updated default paths

--- exotracker.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set USER_HOME=C:\Users\sebas
-call %USER_HOME%\miniconda3\Scripts\activate exotracker
-python main.py
-call conda deactivate
--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-filename = 'D:/Projects/UPF/Widefield_TIRF/_TestImages/Test_5x5_pre_C1.tif'
-filename2 = 'D:/Projects/UPF/Widefield_TIRF/_TestImages/Test_5x5_pre_C2.tif'
+filename = 'D:/Projects/UPF/TIRF/_TestImages/Test_5x5_pre_C1.tif'
+filename2 = 'D:/Projects/UPF/TIRF/_TestImages/Test_5x5_pre_C2.tif'
 start_frame, end_frame = 25, 525
 color_codes = ['yellow', 'red', 'green']
