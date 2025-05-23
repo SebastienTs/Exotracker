2f2dcee88e47098b407c8f90d59076611720d128
Seb <sebastien.tosi@gmail.com>
2025-05-23 18:29:55 +0200

    - Metadata management - Tabifyed data analysis widget - Slight parameter updates - Code cleanup

--- algos.py
+++ algos.py
@@ -152,11 +152,11 @@ def track_spots_trackpy(vw: Viewer, max_spot_mean_scale = 1.33, search_range=2, 
           min_neighbor_dist={'widget_type': 'IntSlider', 'max': 10, 'tooltip': 'Minimum distance to closest point in any other track (pixels)'},
           min_c1_contrast={'widget_type': 'FloatSlider', 'max': 0.5, 'step': 0.001, 'tooltip': 'C1 minimum average contrast'},
           min_c2_contrast_delta={'widget_type': 'FloatSlider', 'max': 0.5, 'step': 0.001, 'tooltip': 'C2 minimum contrast variation'},
-          track_c2_int_medrad={'widget_type': 'IntSlider', 'min': 1, 'max': 6, 'tooltip': 'C2 track detection median filter window half length'},
+          track_c2_int_medrad={'widget_type': 'IntSlider', 'min': 1, 'max': 9, 'tooltip': 'C2 track detection median filter window half length'},
           track_c2_int_thr={'widget_type': 'FloatSlider', 'min': 0.25, 'max': 0.5, 'step': 0.01, 'tooltip': 'C2 track detection normalized intensity threshold'})
 def analyze_tracks_int_gate(vw: Viewer, min_startframe=25, min_afterframe=50,
                             min_neighbor_dist=4, min_c1_contrast=0.26, min_c2_contrast_delta=0.11,
-                            track_c2_int_medrad=4, track_c2_int_thr=0.35) -> LayerDataTuple:
+                            track_c2_int_medrad=5, track_c2_int_thr=0.35) -> LayerDataTuple:
 
   if viewer_is_layer(vw, 'Tracks') and viewer_is_layer(vw, 'Blobs'):
 
--- graphs.py
+++ graphs.py
@@ -10,14 +10,16 @@ from settings import frame_timestep, proteins
 from algos import *
 warnings.filterwarnings("ignore")
 
-reanalyze = False       # Recompute C2 tracks from intensity gating (debug, bypass posterior plots and analysis)
-model = False           # Estimate C2 intensity plateaus by fitting logistic functions
-
 # Re-analyze (filtering + longuest plateau search) and plot C2 tracks
-def reanalyze_tracks(tracks_props, medrad, trck_thr, first_trck, last_trck):
+def reanalyze_tracks(tracks_props, protein, medrad, trck_thr, first_trck, last_trck):
+
+    # Close all plots
+    plt.close('all')
+
+    # Plot loop
     cnt = 1
     for key, value in list(tracks_props.items())[:]:
-        if tracks_props[key]['ch2_positive'] == 1:
+        if tracks_props[key]['protein2'] == protein and tracks_props[key]['ch2_positive'] == 1:
             if cnt >= first_trck and cnt <= last_trck:
                 plt.figure()
                 start, end, _ , vals_flt, _ = estimate_track_lgth(tracks_props[key]['ch2_ext_int'], medrad, trck_thr)
@@ -26,17 +28,22 @@ def reanalyze_tracks(tracks_props, medrad, trck_thr, first_trck, last_trck):
                 vals_flt[end:] = np.NaN
                 plt.plot(vals_flt, color=plt.gca().get_lines()[-1].get_color())
                 plt.grid()
-                plt.title(f'C1 Track {cnt}')
+                plt.title(f'C2 Track {cnt} from {protein}')
             cnt += 1
     tile_windows(300, 200)
     plt.show()
 
 ## Fit a 2 up-step, 1 down-step logistic function to C2 intensity profiles
-def fit_plateaus(tracks_props, timestep, first_trck, last_trck):
+def fit_plateaus(tracks_props, protein, timestep, first_trck, last_trck):
+
+    # Close all plots
+    plt.close('all')
+
+    # Plot loop
     cnt = 1
     for key, value in list(tracks_props.items())[:]:
         if tracks_props[key]['ch2_positive'] == 1:
-            if cnt >= first_trck and cnt <= last_trck:
+            if tracks_props[key]['protein2'] == protein and cnt >= first_trck and cnt <= last_trck:
                 plt.figure()
                 int_c2 = tracks_props[key]['ch2_ext_int']
                 int_c2 = (int_c2-min(int_c2))/(max(int_c2)-min(int_c2))
@@ -49,17 +56,24 @@ def fit_plateaus(tracks_props, timestep, first_trck, last_trck):
                 plt.plot(x, model_logistic(x, *popt), 'r-', label='Fit')
                 plt.vlines(x=xpos, ymin=0, ymax=1, colors='green', linestyles='dashed')
                 plt.xlim(0, len(int_c2)*timestep)
-                plt.title(f'C1 Track {cnt}')
+                plt.title(f'C2 Track {cnt} from {protein}')
             cnt += 1
     tile_windows(300, 200)
     plt.show()
 
 # Plot filtered intensity profiles of C1-C2 track pairs
-def  plot_tracks_intensity(tracks_props, tracks_c2_times, first_trck, last_trck, medrad, int_norm, int_preframe, int_postframe):
+def  plot_tracks_intensity(tracks_props, tracks_c2_times, protein, first_trck, last_trck, medrad, int_norm):
+
+    # Close all plots
+    plt.close('all')
+
+    # Plot loop
     cnt = 1
     for key, value in list(tracks_props.items())[:]:
-        if tracks_props[key]['ch2_positive'] == 1:
+        if tracks_props[key]['protein2'] == protein and tracks_props[key]['ch2_positive'] == 1:
             if cnt >= first_trck and cnt <= last_trck:
+                int_preframe = tracks_props[key]['int_preframe']
+                int_postframe = tracks_props[key]['int_postframe']
                 # Median filter
                 int_c1 = medfilt(tracks_props[key]['ch1_ext_int'], kernel_size=2*medrad+1)
                 int_c2 = medfilt(tracks_props[key]['ch2_ext_int'], kernel_size=2*medrad+1)
@@ -80,14 +94,15 @@ def  plot_tracks_intensity(tracks_props, tracks_c2_times, first_trck, last_trck,
                 plt.plot(int_c2, color='green')
                 #plt.plot(ctr_c2, color='blue')
                 plt.grid()
-                plt.title(f'C1 Track {cnt}')
+                plt.title(f'C2 Track {cnt} from {protein}')
             cnt += 1
     tile_windows(300, 200)
     plt.show(block=False)
 
 
 # Plot C1/C2 averaged intensity time profiles + std
-def plot_tracks_avg_intensity(tracks_props, medrad, int_norm, proteins, int_preframe):
+def plot_tracks_avg_intensity(tracks_props, protein, medrad, int_norm):
+
     # C1 track intensity profile is resampled to a vector of fixed size (rsplgth)
     # C2 buffer is larger to accomodate a shift (rspgth) and pre-/post- frames (assumed not exceeding 3x C1 track length)
     rsplgth = 256
@@ -95,7 +110,8 @@ def plot_tracks_avg_intensity(tracks_props, medrad, int_norm, proteins, int_pref
     arr_int_c2 = np.full((int(4*rsplgth), len(tracks_props)), np.nan)
     cnt = 1
     for key, value in list(tracks_props.items())[:]:
-        if tracks_props[key]['ch2_positive'] == 1:
+        if tracks_props[key]['protein2'] == protein and tracks_props[key]['ch2_positive'] == 1:
+            int_preframe = tracks_props[key]['int_preframe']
             # Load C1 and C2 intensity profiles
             int_c1 = tracks_props[key]['ch1_int']
             int_c2 = tracks_props[key]['ch2_ext_int']
@@ -131,8 +147,8 @@ def plot_tracks_avg_intensity(tracks_props, medrad, int_norm, proteins, int_pref
 
     # Plot
     fig = plt.figure()
-    plt.plot(np.arange(0, 1, 1/rsplgth), avg_int_c1, color='red', label=proteins[0])
-    plt.plot(np.arange(-1, 3, 1/rsplgth), avg_int_c2, color='green', label=proteins[1])
+    plt.plot(np.arange(0, 1, 1/rsplgth), avg_int_c1, color='red', label='exo84')
+    plt.plot(np.arange(-1, 3, 1/rsplgth), avg_int_c2, color='green', label=protein)
     fig.gca().fill_between(np.arange(0, 1, 1/rsplgth), avg_int_c1-std_int_c1, avg_int_c1+std_int_c1, color='red', alpha=0.2)
     fig.gca().fill_between(np.arange(-1, 3, 1/rsplgth), avg_int_c2-std_int_c2, avg_int_c2+std_int_c2, color='green', alpha=0.2)
     plt.xlim(-mx_prefrc, 1+mx_postfrc)
@@ -177,38 +193,35 @@ def plot_tracks_timelines(data_list, proteins, timestep):
 
 @magicgui(call_button='Analyze',
           groupfiles={'widget_type': 'Checkbox', 'tooltip': 'Process all .pkl files from current image folder'},
+          protein={'widget_type': 'LineEdit', 'tooltip': 'C2 protein name (used as filter for intensity plots)'},
+          model={'widget_type': 'Checkbox', 'tooltip': 'Model C2 tracks intensity profiles'},
+          medrad={'widget_type': 'IntSlider', 'min': 1, 'max': 9, 'tooltip': 'C2 track detection median filter window half length'},
+          trck_thr={'widget_type': 'FloatSlider', 'min': 0.25, 'max': 0.5, 'step': 0.01, 'tooltip': 'C2 track detection normalized intensity threshold'},
+          reanalyze={'widget_type': 'Checkbox', 'tooltip': 'Re-analyze C2 tracks based on intensity profiles'},
           plot_intprofiles={'widget_type': 'Checkbox', 'tooltip': 'Plot intensity profiles'},
           plot_first_trck={'widget_type': 'IntSlider', 'min': 1, 'max': 250, 'tooltip': 'First track to plot'},
           plot_last_trck={'widget_type': 'IntSlider', 'min': 1, 'max': 250, 'tooltip': 'Last track to plot'},
           plot_avg_intprofile={'widget_type': 'Checkbox', 'tooltip': 'Plot average intensity profile'},
           plot_timelines={'widget_type': 'Checkbox', 'tooltip': 'Plot timelines'})
-def graph_tracks(groupfiles=False, plot_intprofiles=False, plot_first_trck=1, plot_last_trck=25, plot_avg_intprofile=True, plot_timelines=True):
+def graph_tracks(groupfiles=False, protein='sec9', model=False, medrad=5, trck_thr=0.35, reanalyze=False,
+                 plot_intprofiles=False, plot_first_trck=1, plot_last_trck=25, plot_avg_intprofile=True, plot_timelines=True):
 
-    # Close all plots
-    plt.close('all')
+    # Results files have same names as C1/C2 images but .pkl extension
+    files_C1 = [str(load_images_tiff.imagepath.value).replace('.tif', '.pkl')]
+    files_C2 = [str(load_images_tiff.imagepath2.value).replace('.tif', '.pkl')]
 
-    # Identify results files in image folder
-    results_C1 = load_images_tiff.imagepath.value
-    results_C2 = load_images_tiff.imagepath2.value
-    extension = Path(results_C1).suffix
-    newextension = '.pkl'
-    results_C1 = str(results_C1).replace(extension, newextension)
-    results_C2 = str(results_C2).replace(extension, newextension)
+    # List results files in image folder
     if groupfiles:
-        folder = Path(results_C1).parent
-        files_C1 = [str(f) for f in folder.glob(f'*{newextension}') if f.is_file() and '_C1' in f.name]
-        files_C2 = [str(f) for f in folder.glob(f'*{newextension}') if f.is_file() and '_C2' in f.name]
-    else:
-        files_C1 = [results_C1]
-        files_C2 = [results_C2]
+        files_C1 = [str(f) for f in Path(files_C1[0]).parent.glob(f'*{".pkl"}') if f.is_file() and '_C1' in f.name]
+        files_C2 = [str(f) for f in Path(files_C2[0]).parent.glob(f'*{".pkl"}') if f.is_file() and '_C2' in f.name]
 
     # Display analysis information
     print('---------------- Starting data analysis ----------------')
-    print('Analyzing these files: ')
+    print('Analyzing files: ')
     print([Path(file_C1).name for file_C1 in files_C1])
     print([Path(file_C2).name for file_C2 in files_C2])
 
-    # Aggregate selected pkl files to track dictionaries
+    # Aggregate content from pkl files to dictionaries
     tracks_props = {}
     tracks_c2_times = {}
     for i, (file_C1, file_C2) in enumerate(zip(files_C1, files_C2)):
@@ -217,42 +230,29 @@ def graph_tracks(groupfiles=False, plot_intprofiles=False, plot_first_trck=1, pl
         with open(file_C2, 'rb') as file:
             tracks_c2_times.update({str(k) + '_'+str(i): v for k, v in pickle.load(file).items()})
 
-    # Extract metadata
-    proteins = [0, 0]
-    for key, value in list(tracks_props.items())[:]:
-        frame_timestep = tracks_props[key]['frame_timestep']
-        int_preframe = tracks_props[key]['int_preframe']
-        int_postframe = tracks_props[key]['int_postframe']
-        proteins[0] = tracks_props[key]['protein1']
-        proteins[1] = tracks_props[key]['protein2']
-        break
-    print(f'Frame timestep: {frame_timestep} s')
-    print(f'Number of preframe(s): {int_preframe}')
-    print(f'Number of postframe(s): {int_postframe}')
-    print(f'Proteins: {proteins}')
-
     ## Statistics over all C1/C2 tracks
     print(f'Number of C2 positive C1 tracks: {len(tracks_c2_times.items())}')
-    starts_c1, starts_c2 = [], []
-    ends_c1, ends_c2 = [], []
-    lgths_c1, lgths_c2 = [], []
+    starts_c1, starts_c2 = [[] for _ in range(2)]
+    ends_c1, ends_c2 = [[] for _ in range(2)]
+    lgths_c1, lgths_c2 = [[] for _ in range(2)]
     for key, trck_times in tracks_c2_times.items():
+        int_preframe = tracks_props[key]['int_preframe']
         lgths_c1.append(tracks_props[key]['length'])
         starts_c2.append(trck_times[0]-int_preframe)
         ends_c2.append(trck_times[1]-int_preframe)
         lgths_c2.append(trck_times[2])
     print(f'C1 mean track length: {np.mean(lgths_c1)*frame_timestep:.2f} +/- {np.std(lgths_c1)*frame_timestep:.2f} [range: {np.min(lgths_c1)*frame_timestep:.2f} - {np.max(lgths_c1)*frame_timestep:.2f}]')
     print(f'C2 mean track length: {np.mean(lgths_c2)*frame_timestep:.2f} +/- {np.std(lgths_c2)*frame_timestep:.2f} [range: {np.min(lgths_c2)*frame_timestep:.2f} - {np.max(lgths_c2)*frame_timestep:.2f}]')
-    data_list = [[np.zeros_like(lgths_c1), lgths_c1], [starts_c2, ends_c2]]
 
     # Re-analyze C2 tracks
     if reanalyze:
-        reanalyze_tracks(tracks_props, medrad = 4, trck_thr = 0.35, first_trck=plot_first_trck, last_trck=plot_last_trck)
+        reanalyze_tracks(tracks_props, protein=protein, medrad=medrad, trck_thr=trck_thr, first_trck=plot_first_trck, last_trck=plot_last_trck)
     if model:
-        fit_plateaus(tracks_props, timestep=frame_timestep, first_trck=plot_first_trck, last_trck=plot_last_trck)
+        fit_plateaus(tracks_props, protein=protein, timestep=frame_timestep, first_trck=plot_first_trck, last_trck=plot_last_trck)
     if plot_intprofiles:
-        plot_tracks_intensity(tracks_props, tracks_c2_times, first_trck=plot_first_trck, last_trck = plot_last_trck, medrad = 4, int_norm = False, int_preframe = int_preframe, int_postframe = int_postframe)
+        plot_tracks_intensity(tracks_props, tracks_c2_times, protein=protein, first_trck=plot_first_trck, last_trck = plot_last_trck, medrad = 4, int_norm = False)
     if plot_avg_intprofile:
-        plot_tracks_avg_intensity(tracks_props, medrad = 4, int_norm = True, proteins=proteins, int_preframe = int_preframe)
+        plot_tracks_avg_intensity(tracks_props, protein=protein, medrad=medrad, int_norm = True)
     if plot_timelines:
-        plot_tracks_timelines(data_list, proteins=proteins, timestep=frame_timestep)
+        data_list = [[np.zeros_like(lgths_c1), lgths_c1], [starts_c2, ends_c2]]
+        plot_tracks_timelines(data_list, proteins=['exo84', 'sec9'], timestep=frame_timestep)
