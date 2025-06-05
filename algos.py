- Use fixed short moving median filter in average intensity plots (instead of using medrad for filter length) - Force min_startframe and min_afterframe to maximum values that are respectively equal to the minimum track duration and twice this duration (average intensity plot requirement) to avoid error - Option to estimate C2 tracks timelines from median instead of mean - Remove early C2 track filter in Trinity exporter - Min/max values updates in spot tracking widget - Display average spots detected per frame with 1 decimal instead as integer - Documentation updates

algos.py
@@ -90,8 +90,9 @@ def detect_spots_msdog(vw: Viewer, spot_rad=2, detect_thr=0.3) -> LayerDataTuple
             blb_scales = blb_scales + list(blb_kept_scales)
 
         # Display blob count
-        print(f'Detected {len(blb_lst)} spots ({np.round(len(blb_lst)/img.shape[0])}/frame)')
-        detect_spots_msdog.call_button.text = f'{len(blb_lst)} Spots ({np.round(len(blb_lst)/img.shape[0])}/frame)'
+        spot_avg_frame = np.round(10*len(blb_lst)/img.shape[0])/10
+        print(f'Detected {len(blb_lst)} spots ({spot_avg_frame}/frame)')
+        detect_spots_msdog.call_button.text = f'{len(blb_lst)} Spots ({spot_avg_frame}/frame)'
 
         # Return napari point layer
         return ([blob[:3] for blob in blb_lst],
@@ -108,13 +109,13 @@ def detect_spots_msdog(vw: Viewer, spot_rad=2, detect_thr=0.3) -> LayerDataTuple
 ## TrackPy + parametric filter
 
 @magicgui(call_button='Track',
-          max_spot_mean_scale={'widget_type': 'FloatSlider', 'min': 1, 'max': 2.5, 'tooltip': 'Track maximum mean spot scale (pixels)'},
+          max_spot_mean_scale={'widget_type': 'FloatSlider', 'min': 1, 'max': 2.5, 'tooltip': 'Maximum track mean spot scale (pixels)'},
           search_range={'widget_type': 'IntSlider', 'min': 1, 'max': 5, 'tooltip': 'Spot search range (pixels)'},
-          max_gap={'widget_type': 'IntSlider', 'max': 20, 'tooltip': 'Track maximum gap (frames)'},
-          min_duration={'widget_type': 'IntSlider', 'min': 3, 'max': 50, 'tooltip': 'Track minimum duration (frames)'},
-          max_duration={'widget_type': 'IntSlider', 'min': 50, 'max': 500, 'tooltip': 'Track maximum duration (frames)'},
-          min_length={'widget_type': 'FloatSlider', 'max': 5, 'tooltip': 'Track minimum accumulated length (pixels)'},
-          max_mean_speed={'widget_type': 'FloatSlider', 'max': 5, 'tooltip': 'Track maximum average speed (pixels/frame)'})
+          max_gap={'widget_type': 'IntSlider', 'max': 20, 'tooltip': 'Maximum track gap (frames)'},
+          min_duration={'widget_type': 'IntSlider', 'min': 5, 'max': 50, 'tooltip': 'Minimum track duration (frames)'},
+          max_duration={'widget_type': 'IntSlider', 'min': 50, 'max': 250, 'tooltip': 'Maximum track duration (frames)'},
+          min_length={'widget_type': 'FloatSlider', 'max': 5, 'tooltip': 'Minimum track length (pixels)'},
+          max_mean_speed={'widget_type': 'FloatSlider', 'max': 5, 'tooltip': 'Maximum track average speed (pixels/frame)'})
 def track_spots_trackpy(vw: Viewer, max_spot_mean_scale = 1.33, search_range=2, max_gap=15, min_duration=35, max_duration=250, min_length=0, max_mean_speed=0.2) -> LayerDataTuple:
 
     if viewer_is_layer(vw, 'Blobs'):
@@ -160,15 +161,24 @@ def track_spots_trackpy(vw: Viewer, max_spot_mean_scale = 1.33, search_range=2, 
 ## Parametric Filter + C2 Intensity Gating
 
 @magicgui(call_button='Filter and Save Tracks',
-          min_startframe={'widget_type': 'IntSlider', 'max': 50, 'tooltip': 'Earliest track start (frame index)'},
-          min_afterframe={'widget_type': 'IntSlider', 'max': 50, 'tooltip': 'Latest track end (number of frames before last frame)'},
+          min_startframe={'widget_type': 'IntSlider', 'max': 25, 'tooltip': 'Minimum track start frame (window used for C2 intensity analysis)'},
+          min_afterframe={'widget_type': 'IntSlider', 'max': 50, 'tooltip': 'Maximum track end before last frame (window used for C2 intensity analysis)'},
           min_neighbor_dist={'widget_type': 'IntSlider', 'max': 10, 'tooltip': 'Minimum distance to closest point in any other track (pixels)'},
           min_c1_contrast={'widget_type': 'FloatSlider', 'max': 0.5, 'step': 0.001, 'tooltip': 'C1 minimum average contrast'},
           min_c2_contrast_delta={'widget_type': 'FloatSlider', 'max': 0.5, 'step': 0.001, 'tooltip': 'C2 minimum contrast variation'},
           track_c2_int_medrad={'widget_type': 'IntSlider', 'min': 1, 'max': 9, 'tooltip': 'C2 track detection median filter window half length'},
           track_c2_int_thr={'widget_type': 'FloatSlider', 'min': 0.25, 'max': 0.5, 'step': 0.01, 'tooltip': 'C2 track detection normalized intensity threshold'})
-def analyze_tracks_int_gate(vw: Viewer, min_startframe=25, min_afterframe=50, min_neighbor_dist=4, min_c1_contrast=0.26,
+def analyze_tracks_int_gate(vw: Viewer, min_startframe=10, min_afterframe=20, min_neighbor_dist=4, min_c1_contrast=0.26,
                             min_c2_contrast_delta=0.15, track_c2_int_medrad=5, track_c2_int_thr=0.35) -> LayerDataTuple:
+
+  if min_startframe > track_spots_trackpy.min_duration.value:
+      analyze_tracks_int_gate.min_startframe.value = track_spots_trackpy.min_duration.value
+      min_startframe = track_spots_trackpy.min_duration.value
+      print('Minimum start frame cannot be larger than minimum track duration!')
+  if min_afterframe > 2*track_spots_trackpy.min_duration.value:
+      print('Minimum after frame cannot be larger than twice the minimum track duration!')
+      min_afterframe = 2*track_spots_trackpy.min_duration.value
+      analyze_tracks_int_gate.min_afterframe.value = 2*track_spots_trackpy.min_duration.value
 
   if viewer_is_layer(vw, 'Tracks') and viewer_is_layer(vw, 'Blobs'):
 

graphs.py
@@ -85,7 +85,10 @@ def  plot_tracks_intensity(tracks_props, tracks_c2_times, proteins, first_trck, 
 def plot_tracks_avg_intensity(tracks_props, proteins, medrad, int_norm):
 
     # C1 track intensity profile is resampled to a vector of fixed size (rsplgth)
-    # C2 buffer is larger to accomodate a shift (rspgth) and pre-/post- frames (assumed not exceeding 3x C1 track length)
+    # Besides C1 track length, C2 buffer is dimensioned to accomodate the pre-frames and post-frames
+    # Calling N the minimum C1 track duration:
+    # - The number of pre-frames shouldn't exceed N
+    # - The number of post-frames shouldn't exceed 2xN
     rsplgth = 256
     arr_int_c1 = np.full((int(1*rsplgth), len(tracks_props)), np.nan)
     arr_int_c2 = np.full((int(4*rsplgth), len(tracks_props)), np.nan)
@@ -99,8 +102,8 @@ def plot_tracks_avg_intensity(tracks_props, proteins, medrad, int_norm):
             c1_lgth = len(int_c1)
             c2_lgth = len(int_c2)
             # Median filter intensity profiles
-            int_c1 = medfilt(int_c1, kernel_size=2*medrad+1)
-            int_c2 = medfilt(int_c2, kernel_size=2*medrad+1)
+            int_c1 = medfilt(int_c1, kernel_size=5)
+            int_c2 = medfilt(int_c2, kernel_size=5)
             # Normalize intensity profiles
             if int_norm:
                 int_c1 = (int_c1-min(int_c1))/(max(int_c1)-min(int_c1))
@@ -139,16 +142,22 @@ def plot_tracks_avg_intensity(tracks_props, proteins, medrad, int_norm):
 
 
 # Plot proteins timelines (mean start/end + std)
-def plot_tracks_timelines(data_list, proteins):
+def plot_tracks_timelines(data_list, proteins, use_median):
     cols = ['red', 'green', 'blue', 'orange', 'pink', 'cyan', 'yellow']
     n = len(data_list)
     figsize = (6,3*n)
     fig, ax = plt.subplots(1, 1, figsize=figsize, sharex=True)
     for i, data in enumerate(data_list):
         # Compute average +std track start / end
-        start = np.mean(data[0])
+        if use_median:
+            start = np.median(data[0])
+        else:
+            start = np.mean(data[0])
         start_std = np.std(data[0])
-        end = np.mean(data[1])
+        if use_median:
+            end = np.median(data[1])
+        else:
+            end = np.mean(data[1])
         end_std = np.std(data[1])
         if i==0:
             startref = start
@@ -171,7 +180,7 @@ def plot_tracks_timelines(data_list, proteins):
     plt.title('Track timelines')
     plt.show(block=False)
 
-def trinity_exporter(tracks_props, tracks_c2_times, exclude_earlyc2, exportpath, proteins):
+def trinity_exporter(tracks_props, tracks_c2_times, exportpath, proteins):
 
     print('---- Trinity Exporter ----')
 
@@ -215,7 +224,7 @@ def trinity_exporter(tracks_props, tracks_c2_times, exclude_earlyc2, exportpath,
                 end_c2 = (trck_times[1]-tracks_props[key]['int_preframe'])
                 lgth_c2 = trck_times[2]
 
-                if  end_c2>=end_c1 or not exclude_earlyc2:
+                if  end_c2>=end_c1:
                     ALL_EXO = max(end_c1, end_c2) - min(start_c1, start_c2)
                     ALL_C1 = lgth_c1
                     ALL_C2 = lgth_c2
@@ -279,11 +288,11 @@ def trinity_exporter(tracks_props, tracks_c2_times, exclude_earlyc2, exportpath,
           plot_average_intensity_profile={'widget_type': 'Checkbox', 'tooltip': 'Plot average intensity profile for current file(s) and protein conditions'},
           intnorm={'widget_type': 'Checkbox', 'tooltip': 'Normalize intensity'},
           plot_timelines={'widget_type': 'Checkbox', 'tooltip': 'Plot timelines'},
-          export_to_trinity={'widget_type': 'Checkbox', 'tooltip': 'Export C1 and C2 track lengths to Trinity format'},
-          exclude_earlyc2={'widget_type': 'Checkbox', 'tooltip': 'Exclude C2 tracks ending before C1 tracks'})
+          median_start_end={'widget_type': 'Checkbox', 'tooltip': 'Use median instead of mean for C2 starts/ends'},
+          export_to_trinity={'widget_type': 'Checkbox', 'tooltip': 'Export C1 and C2 track lengths to Trinity format'})
 def graph_tracks(groupfiles=False, model=False, plot_intensity_profiles=False, plot_first_trck=1, plot_last_trck=25,
-                 plot_average_intensity_profile=True, intnorm=True, plot_timelines=False, export_to_trinity=False,
-                 exclude_earlyc2=True):
+                 plot_average_intensity_profile=True, intnorm=True, plot_timelines=False, median_start_end=False,
+                 export_to_trinity=False):
 
     # Proteins of the current dataset
     proteins_str = load_images_tiff.proteins.value
@@ -362,13 +371,13 @@ def graph_tracks(groupfiles=False, model=False, plot_intensity_profiles=False, p
                     proteins.append(protein2)
 
             # Call plotting function
-            plot_tracks_timelines(data_list, proteins=proteins)
+            plot_tracks_timelines(data_list, proteins=proteins, use_median=median_start_end)
 
         if export_to_trinity:
             exportpath = path.dirname(load_images_tiff.imagepath.value)+'/Trinity/'
             if not path.exists(exportpath):
                 makedirs(exportpath)
-            trinity_exporter(tracks_props, tracks_c2_times, exclude_earlyc2, exportpath, proteins=proteins)
+            trinity_exporter(tracks_props, tracks_c2_times, exportpath, proteins=proteins)
         else:
             graph_tracks.call_button.text = 'Process'
 